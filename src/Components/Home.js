fabd8dd2af34edb086d283ef92e07135e88a3b90
Evolution_KK <karankundra1580@gmail.com>
2022-01-29 18:10:54 +0530

    Hellooooo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,19 +7,17 @@ import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [show, setShow] = useState(false);
-  const [image, setImage] = useState("")
+  const [image, setImage] = useState("");
+  const [Input, setInput] = useState("");
+  const [show1, setShow1] = useState(false);
+  const [data, setData] = useState([])
+  const [isError, setIsError] = useState(false);
+  const [search, setSearch] = useState("")
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const [show1, setShow1] = useState(false);
   const handleClose1 = () => setShow1(false);
   const handleShow1 = () => setShow1(true);
 
-  const [isError, setIsError] = useState(false);
-
-  const [data, setData] = useState([])
-
-  const [search, setSearch] = useState("")
   useEffect(() => {
     const fetchData = async () => {
       setIsError(false)
@@ -39,13 +37,13 @@ export default function Home() {
     fetchData();
   }, []);
 
-  const imageSelect = () => {
-    setImage(data)
-    handleClose1(false)
-    console.log(image)
-  }
   const renderGif = () => {
     return data.map(eL => {
+      const imageSelect = () => {
+        setImage(eL.images.fixed_height.url)
+        handleClose1(false)
+        console.log(image)
+      }
       return (
         <div key={eL.id} className='gif'>
           <img src={eL.images.fixed_height.url} alt="" value={image} onClick={imageSelect} />
@@ -53,9 +51,11 @@ export default function Home() {
       )
     })
   }
+
   const handleSearch = (event) => {
     setSearch(event.target.value)
   }
+
   const handleSubmit = async event => {
     event.preventDefault();
     setIsError(false)
@@ -70,9 +70,21 @@ export default function Home() {
       setData(results.data.data);
     } catch (err) {
       setIsError(true)
-      console.log(err)
     }
   };
+
+  const handleInput = (event) => {
+    setInput(event.target.value)
+  }
+  const handlePost = () => {
+    setShow(false)
+    return (
+      <div>
+        <p>{Input}</p>
+        <img src={image} alt="" />
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -80,18 +92,20 @@ export default function Home() {
       <div className='card'>
         <ul>
           <li><button className='home-button'>Find Friends</button></li>
-          <li><button className='home-button'>Group</button></li>
-          <li><button className='home-button'>Watch</button></li>
-          <li><button className='home-button'>Memories</button></li>
-          <li><button className='home-button'>Events</button></li>
-          <li><button className='home-button'>Favourite</button></li>
+          <li><button style={{marginTop: "6rem"}} className='home-button'>Group</button></li>
+          <li><button style={{marginTop: "12rem"}} className='home-button'>Watch</button></li>
+          <li><button style={{marginTop: "18rem"}} className='home-button'>Memories</button></li>
+          <li><button style={{marginTop: "24rem"}} className='home-button'>Events</button></li>
+          <li><button style={{marginTop: "30rem"}} className='home-button'>Favourite</button></li>
         </ul>
+        <div className='post-text'>
+          <h3 style={{marginTop: "8rem"}}>{Input}</h3>
+          <img className='post-img' style={{marginTop: "0.5rem"}} src={image} alt="" />
+        </div>
       </div>
-
       <div className='home-input'>
         <Button onClick={handleShow} className='post-home'>What's on Your Mind ?</Button>
       </div>
-
       <Modal className="modal" show={show} onHide={handleClose}>
         <Modal.Header closeButton className='modal-title'>
           <Modal.Title  >
@@ -100,8 +114,7 @@ export default function Home() {
             <button style={{ border: "none" }} className='modal-title-button'>Photo / Video Album</button>
           </Modal.Title>
         </Modal.Header>
-
-        <input className="modal-input" placeholder='Type here...'></input>
+        <input type="text" value={Input} onChange={handleInput} className="modal-input" placeholder='Type here...'></input>
         <img src={image} alt="" />
         <Modal.Footer>
           <Button variant="secondary" style={{ marginRight: "8.5rem", width: "6rem", borderRadius: '1rem', position: "absolute" }}>
@@ -117,15 +130,11 @@ export default function Home() {
           <Button variant="secondary" style={{ marginleft: "", width: "7rem", borderRadius: '1rem' }}>
             Tag Event
           </Button>
-
         </Modal.Footer>
-        <Button variant="primary" style={{ marginLeft: "25rem", marginBottom: "1rem", width: "5rem" }} onClick={handleClose}>
+        <Button variant="primary" style={{ marginLeft: "25rem", marginBottom: "1rem", width: "5rem" }} onClick={handlePost}>
           Post
         </Button>
       </Modal>
-      {/* <div className="postcard">
-        <input className="home-input" placeholder="What's on your mind?"></input>
-      </div> */}
       <Modal className="modale" show={show1} onHide={handleClose1}>
         <Modal.Title style={{ textAlign: "center" }} >
           GIF
@@ -144,6 +153,11 @@ export default function Home() {
           <Button onClick={handleClose1} style={{ marginLeft: "1rem" }}>Close</Button>
         </Modal.Footer>
       </Modal>
+      {/* <div>
+        <p>{Input}</p>
+        <img src={image} alt="" />
+      </div> */}
+
     </div>
   );
 }
